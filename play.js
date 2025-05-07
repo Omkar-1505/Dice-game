2f98fe6b91df90d6de1044b16359cc3cd054cb36
Omkar-1505 <omkarpadheetheboss@gmail.com>
2025-05-07 16:15:23 +0530

    fixing js file

--- play.js
+++ play.js
@@ -1,1 +1,23 @@
 var x = Math.floor(Math.random()*6) + 1;
+var
+
+
+
+
+
+
+var x = Math.floor(Math.random()*6) + 1;
+
+
+
+
+
+if (x===y){
+    document.querySelector(".results").innerHTML="IT IS A DRAW";
+}
+else if (x>y){
+    document.querySelector(".results").innerHTML="PLAYER 1 WINS THE MATCH";
+}
+else{
+    document.querySelector(".results").innerHTML="PLAYER 2 WINS THE MATCH";
+}
